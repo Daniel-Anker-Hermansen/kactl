28a1e45b3c32db574f635803d26e3f84d389fe05
FrederikH22R <43875606+frederikvase@users.noreply.github.com>
2025-10-06 13:20:24 +0200

    new SCC

--- content/graph/SCC.h
+++ content/graph/SCC.h
@@ -1,41 +1,37 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-28
- * License: CC0
- * Source: Czech graph algorithms book, by Demel. (Tarjan's algorithm)
- * Description: Finds strongly connected components in a
- * directed graph. If vertices $u, v$ belong to the same component,
- * we can reach $u$ from $v$ and vice versa.
- * Usage: scc(graph, [\&](vi\& v) { ... }) visits all components
- * in reverse topological order. comp[i] holds the component
- * index of a node (a component only has edges to components with
- * lower index). ncomps will contain the number of components.
+ * Author: Frederik Vase Haraldsson
+ * Date: 2025-10-06
+ * Source: Kosaraju's algorithm 
+ * Usage: comp[u] == comp[v] iff verticies u and v are in the same connected component.
+ *  comp is numbered from 1 to #components inclusive
  * Time: O(E + V)
- * Status: Bruteforce-tested for N <= 5
+ * Status: Tested on https://cses.fi/problemset/task/1683
  */
 #pragma once
 
-vi val, comp, z, cont;
-int Time, ncomps;
-template<class G, class F> int dfs(int j, G& g, F& f) {
-	int low = val[j] = ++Time, x; z.push_back(j);
-	for (auto e : g[j]) if (comp[e] < 0)
-		low = min(low, val[e] ?: dfs(e,g,f));
+struct SCC {
+    ll n; vvl adj, radj;
+    vi vis, order, comp;
 
-	if (low == val[j]) {
-		do {
-			x = z.back(); z.pop_back();
-			comp[x] = ncomps;
-			cont.push_back(x);
-		} while (x != j);
-		f(cont); cont.clear();
-		ncomps++;
-	}
-	return val[j] = low;
-}
-template<class G, class F> void scc(G& g, F f) {
-	int n = sz(g);
-	val.assign(n, 0); comp.assign(n, -1);
-	Time = ncomps = 0;
-	rep(i,0,n) if (comp[i] < 0) dfs(i, g, f);
-}
+    SCC(vvl &a) : n(sz(a)), adj(a), radj(n), vis(n), comp(n) {
+        rep(i, 0, sz(a)) for (ll e : adj[i]) {
+            radj[e].push_back(i);
+        }
+        rep(i, 0, sz(vis)) if (!vis[i]) dfs1(i);
+        ll j = 0;
+        rep(i, 0, n) {
+            ll v = order[n-i-1];
+            if (!comp[v]) dfs2(v, ++j);
+        } // j equals total number of components
+    }
+    void dfs1(ll v) {
+        vis[v] = 1;
+        for (ll u : adj[v]) if (!vis[u]) dfs1(u);
+        order.push_back(v);
+    }
+    void dfs2(ll v, ll ci) {
+        comp[v] = ci;
+        for (ll u : radj[v]) if (!comp[u]) dfs2(u, ci);
+    }
+};
+
