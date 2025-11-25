2208e10d14ccf39a6199cfb801ae9c9ab98f2965
Sebastian Meineche Falk <202407373@post.au.dk>
2025-11-25 17:20:55 +0100

    "Max" and "Add" augments

--- content/data-structures/Augments.h
+++ content/data-structures/Augments.h
@@ -5,17 +5,31 @@
  * Description: Augments for LazySegmentTree and LazyTreap
  * Status: stress-tested
  */
-
 #pragma once
 
+// Data
+
+struct Max {
+	ll x = LONG_LONG_MIN; // identity
+	Max() {} Max(ll x) : x(x) {}
+	Max operator+(Max t) const { return Max(max(x, t.x)); }
+};
+
 // Updates
+
+struct Add {
+	ll a = 0;
+	Add() {} Add(ll a) : a(a) {}
+	Add operator*(Add u) const { return Add(a + u.a); } // 2. * 1.
+	Max apply(Max t, ll s) const { return Max(t.x + a); }
+};
 
 struct SetAdd {
 	ll set, add;
  
 	U() : set(-1), add(0) {}
 	U(ll set, ll add) : set(set), add(add) {}
-	U operator*(U t) const {
+	U operator*(U t) const { // 2. * 1.
 		if (set != -1) return U(set, add);
 		return U(t.set, add + t.add);
 	}
