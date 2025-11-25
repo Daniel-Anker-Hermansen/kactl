786c738f27ee5cff6d5d01059672272db20c0a24
Sebastian Meineche Falk <202407373@post.au.dk>
2025-11-25 17:11:56 +0100

    (sparse) lazy segment tree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,75 +1,34 @@
 /**
- * Author: Daniel Anker Hermansen
- * Date: 2025-11-18
- * License: CC0
- * Source: folklore
- * Description: Segment treee for range updates and range queries with associative updates and associative value merges.
- * Time: O(\log N).
- * Status: CSES Range Updates and Sums
+ * Author: Daniel, Frederik & Sebastian
+ * Date: 2025-11-25
+ * Source: Folklore
+ * Description: Data structure to support range updates and queries. T is the storage type and U is the update type.
+ * Status: Tested on CSES "Range Update Queries"
  */
 #pragma once
 
+template<class T, class U>
 struct LazySegmentTree {
-	typedef ll T;
-	typedef Update U;
-	T tunit = 0;
-	U uunit = Update{-1, 0};
-	T f(T l, T r) { return l + r; }
-	T u(T l, U u, ll s) { return u.u(l, s); }
-	U g(U l, U r) { return l.g(r); } // g(l, r)(x) == r(l(x))
-
-	ll n;
-	vector<T> vals;
-	vector<U> upds;
-	vl s;
-	
-	LazySegmentTree(vector<T> &ts) : LazySegmentTree(sz(ts)) {
-		rep(i, 0, sz(ts)) vals[n + i] = ts[i];
-		for (ll i = n; --i;) vals[i] = f(vals[2 * i], vals[2 * i + 1]);
+	ll n, s; vector<T> ts; vector<U> us;
+	LazySegmentTree(ll ss) : n(1ll << (64 - __builtin_clzll(ss))), s(n), ts(2*n), us(2*n) { }
+	LazySegmentTree(vector<T> &tss) : LazySegmentTree(sz(tss)) {
+		copy(all(tss), ts.begin() + n);
+		for (ll i = n; --i;) ts[i] = ts[2*i] + ts[2*i+1];
 	}
-
-	LazySegmentTree(ll ss) : n(1ll << (64 - __builtin_clzll(ss))), vals(2 * n, tunit), upds(2 * n, uunit), s(2 * n, 1) {
-		for (ll i = n; --i;) s[i] = s[2 * i] + s[2 * i + 1];
-	}
-
 	void push(ll i) {
-		ll j = 2 * i;
-		ll k = j + 1;
-		if (j < sz(upds)) upds[j] = g(upds[j], upds[i]), upds[k] = g(upds[k], upds[i]), recalc(j), recalc(k); 
-		else vals[i] = u(vals[i], upds[i], 1);
-		upds[i] = uunit;
+		if (i<n) us[2*i]=us[i]*us[2*i], us[2*i+1]=us[i]*us[2*i+1];
+		ts[i] = us[i].apply(ts[i], 1); us[i] = U();
 	}
-
-	void recalc(ll i) {
-		ll j = 2 * i;
-		ll k = j + 1;
-		if (j < sz(upds)) vals[i] = u(f(vals[j], vals[k]), upds[i], s[i]);
-		else vals[i] = u(vals[i], upds[i], 1), upds[i] = uunit;
+	T query(ll l, ll r, ll i=1) { // [l, r), r <= l -> T()
+		push(i); if (r <= 0 || l >= s) return T();
+		if (l <= 0 && r >= s) return ts[i];
+		s/=2; T v = query(l, r, 2*i) + query(l-s, r-s, 2*i+1);
+		s*=2; return v;
 	}
-
-	T query(ll l, ll r, ll i = 1) { // [l, r) r <= l -> tunit
-		if (r <= 0 || l >= s[i]) return tunit;
-		if (l <= 0 && r >= s[i]) return vals[i];
-		push(i);
-		ll j = 2 * i;
-		ll k = j + 1;
-		ll mid = s[j];
-		return u(f(query(l, r, j), query(l - mid, r - mid, k)), upds[i], s[i]);
-	}
-
-	void update(ll l, ll r, U u, ll i = 1) { // [l, r)
-		if (r <= 0 || l >= s[i]) return;
-		if (l <= 0 && r >= s[i]) {
-			upds[i] = g(upds[i], u);
-			recalc(i);
-			return;
-		}
-		push(i);
-		ll j = 2 * i;
-		ll k = j + 1;
-		ll mid = s[j];
-		update(l, r, u, j);
-		update(l - mid, r - mid, u, k);
-		recalc(i);
+	void update(ll l, ll r, U u, ll i=1) { // [l, r)
+		if (l <= 0 && r >= s) return us[i] = u * us[i], push(i);
+		push(i); if (r <= 0 || l >= s) return;
+		s/=2; update(l, r, u, 2*i); update(l-s, r-s, u, 2*i+1);
+		s*=2; ts[i] = ts[2*i] + ts[2*i+1];
 	}
 };
---
+++ content/data-structures/SparseLazySegmentTree.h
@@ -0,0 +1,39 @@
+/**
+ * Author: Daniel, Frederik & Sebastian
+ * Date: 2025-11-25
+ * Source: Folklore
+ * Description: Sparse data structure to support range updates and queries. T is the storage type and U is the update type.
+ * Status: Tested on CSES "Range Update Queries"
+ */
+#pragma once
+
+template<class T, class U>
+struct SparseLazySegmentTree {
+	struct Node {
+		Node *l = 0, *r = 0;
+		T t = T(); U u = U();
+	} *root = 0;
+	ll s, tl, tr;
+	SparseLazySegmentTree(ll tl, ll tr) : s(1ll << (64 - __builtin_clzll(tr-tl))), tl(tl), tr(tl+s) { } // [tl, tr)
+	void fix(Node *&i) { i = i ? i : new Node(); }
+	void push(Node *&i) {
+		fix(i); // remove rest if not using lazy propagation
+		if (s>1) fix(i->l), fix(i->r), i->l->u = i->u * i->l->u, i->r->u = i->u * i->r->u;
+		i->t = i->u.apply(i->t, 1); i->u = U();
+	}
+	T query(ll l, ll r) { return query(l, r, root); }
+	T query(ll l, ll r, Node *&i) { // [l, r), r <= l -> T()
+		push(i); if (r <= tl || l >= tl+s) return T();
+		if (l <= tl && r >= tl+s) return i->t;
+		s/=2; T v = query(l, r, i->l) + query(l-s, r-s, i->r);
+		s*=2; return v;
+	}
+	void update(ll l, ll r, U u) { update(l, r, u, root); }
+	void update(ll l, ll r, U u, Node *&i) { // [l, r)
+		push(i);
+		if (r <= tl || l >= tl+s) return;
+		if (l <= tl && r >= tl+s) return i->u = u * i->u, push(i);
+		s/=2; update(l, r, u, i->l); update(l-s, r-s, u, i->r);
+		s*=2; i->t = i->l->t + i->r->t;
+	}
+};
